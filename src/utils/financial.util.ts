edba946b322e531da9a4842635b55637c1ccd3dc
ed <ed@email.com>
2024-08-25 09:20:30 -0400

    documentation for the utils

--- src/utils/financial.util.ts
+++ src/utils/financial.util.ts
@@ -1,3 +1,9 @@
+/**
+ * Formats a numeric string to a localized string representation with up to two decimal places.
+ *
+ * @param {string} value - The numeric string to format.
+ * @returns {string} The formatted number string.
+ */
 export function utilFormatNumber(value: string): string {
   const formattedNumber = new Intl.NumberFormat('en-US', {
     minimumFractionDigits: 0,
@@ -6,6 +12,14 @@ export function utilFormatNumber(value: string): string {
   return formattedNumber;
 }
 
+/**
+ * Formats a financial numeric string (e.g., "10k", "2M", "3.5b") to a localized string
+ * with up to two decimal places. The function handles suffixes like 'k', 'm', and 'b'
+ * which represent thousands, millions, and billions respectively.
+ *
+ * @param {string} value - The financial numeric string to format.
+ * @returns {string} The formatted financial number string or an empty string if the input is invalid.
+ */
 export function utilFormatFinancialNumber(value: string): string {
   const regex = /^[0-9]*\.?[0-9]+[kKmMbB]?$/;
   if (value.match(regex) === null) {
